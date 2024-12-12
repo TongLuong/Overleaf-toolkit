ea3f4cd7c860791815bbe1bf59c9aa5e2ad024a1
TongLuong <tong.luong203@hcmut.edu.vn>
2024-12-13 00:39:20 +0700

    update start.py

--- start.py
+++ start.py
@@ -1,9 +1,17 @@
 import subprocess
 
+subprocess.run(["chmod +x bin/init"], shell = False)
 subprocess.run(["bin/init"], shell = False)
+
+subprocess.run(["chmod +x bin/up"], shell = False)
 subprocess.run(["bin/up"], shell = False)
+
+subprocess.run(["chmod +x bin/start"], shell = False)
 subprocess.run(["bin/start"], shell = False)
+
+subprocess.run(["chmod +x bin/shell"], shell = False)
 subprocess.run(["bin/shell"], shell = False)
+
 subprocess.run(["wget https://mirror.clientvps.com/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz"], shell = False)
 subprocess.run(["tar -xf install-tl-unx.tar.gz"], shell = False)
 subprocess.run(["perl install-tl", "i"], shell = False)
