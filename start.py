21cb6da0a37bdb24a7977bf318520709a5c71750
TongLuong <tong.luong203@hcmut.edu.vn>
2024-12-13 09:47:25 +0700

    update start.py

--- start.py
+++ start.py
@@ -1,15 +1,17 @@
 import subprocess, os
 
-subprocess.run(["chmod +x bin/init"], shell = True)
+subprocess.run(["chmod -R 755 bin"], shell = True)
+
+# subprocess.run(["chmod +x bin/init"], shell = True)
 subprocess.run(["bin/init"], shell = True)
 
-subprocess.run(["chmod +x bin/up"], shell = True)
+# subprocess.run(["chmod +x bin/up"], shell = True)
 subprocess.run(["bin/up"], shell = True)
 
-subprocess.run(["chmod +x bin/start"], shell = True)
+# subprocess.run(["chmod +x bin/start"], shell = True)
 subprocess.run(["bin/start"], shell = True)
 
-subprocess.run(["chmod +x bin/shell"], shell = True)
+# subprocess.run(["chmod +x bin/shell"], shell = True)
 subprocess.run(["bin/shell"], shell = True)
 
 subprocess.run(["wget https://mirror.clientvps.com/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz"], shell = True)
